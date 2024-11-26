52c99a33bd305ee9aabd8b2c1ef6ffdfafd0b7e0
jialin <mr.liujalo@gmail.com>
2024-11-26 16:17:10 +0800

    Home New Panel

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,9 +1,10 @@
 <script setup>
-import HomeCategory from './components/HomeCategory.vue'
-import HomeBanner from './components/HomeBanner.vue'
-import HomeNew from './components/HomeNew.vue'
-import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProducts.vue'
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import homeProduct from "./components/HomeProducts.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
@@ -11,7 +12,13 @@ import homeProduct from './components/HomeProducts.vue'
     <HomeCategory />
     <HomeBanner />
   </div>
-  <HomeNew />
-  <HomeHot />
+  <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品"
+    ><HomeNew
+  /></HomePanel>
+  <HomePanel title="人气推荐" subtitle="人气推荐 好多商品"
+    ><HomeHot
+  /></HomePanel>
+
   <homeProduct />
+
 </template>
